76fec4dae104a42d21c60df88a93c8893af2182e
thisisAryaa <aryagem678@gmail.com>
2021-06-12 15:47:05 +0530

    created the slingShot

--- SlingShot.js
+++ SlingShot.js
@@ -1,20 +1,27 @@
-class Chain{
-    constructor(bodyA, bodyB){
+class SlingShot{
+    constructor(bodyA, pointB){
         var options = {
             bodyA: bodyA,
-            bodyB: bodyB,
+            pointB: pointB,
             stiffness: 0.04,
             length: 10
         }
+        this.pointB = pointB; 
         this.chain = Constraint.create(options);
         World.add(world, this.chain);
     }
 
     display(){
+        if(this.chain.bodyA){
+        
         var pointA = this.chain.bodyA.position;
-        var pointB = this.chain.bodyB.position;
+        var pointB = this.pointB;
         strokeWeight(4);
         line(pointA.x, pointA.y, pointB.x, pointB.y);
     }
-    
 }
+    fly(){
+    this.chain.bodyA = null;
+    }
+}
+
